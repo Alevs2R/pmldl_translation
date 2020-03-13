ea86a8d793a3391313621cdf5d3a0ed5130488c5
Aleksei Evlampev <a.evlampev@innopolis.ru>
2020-03-13 13:42:49 +0300

    add pytorch translation

---
+++ translate.py
@@ -0,0 +1,35 @@
+import sys
+import numpy as np
+import string
+import random
+import os
+import time
+
+def get_lines(file):
+    reader = open(file)
+    train_lines = reader.readlines()
+    reader.close()
+
+    return train_lines
+
+lines = get_lines('test.ru.txt') 
+
+from fairseq.models.transformer import TransformerModel
+ru2en = TransformerModel.from_pretrained(
+  '/home/aleksei/Documents/wmt19.ru-en.ensemble',
+  checkpoint_file='model1.pt'
+)
+
+result_lines = []
+for line in lines:
+    translated = ru2en.translate(line)
+    print(translated)
+    result_lines.append(translated)
+
+print('translated, saving to file...')
+
+with open('output.txt', 'a') as the_file:
+    for line in result_lines:
+        the_file.write('%s\n' % line)
+
+print('saved')
